24ea1e7476685930a9865be9c233e8f805f1b9e4
apple <c1200922@st.kanazawa-it.ac.jp>
2023-11-03 08:56:08 +0900

    json

--- index.py
+++ index.py
@@ -1,1 +1,3 @@
-print("konnnitiha")
+import json
+file = open("info.py", "r")
+info = json.load(file)
--- info.py
+++ info.py
@@ -1,5 +1,5 @@
 from json import *
 {
-    'CHANNEL_ACCESS_TOKEN': 'XuQz/EHfB2jodhYK4zZNvn6Wzual/YbnM8yLQGLmUR/cy7nsCC5uV2JN2bU4KCZzT7Yy2rO9yxfxY6isPyr2W6KQSReqfamLieRlxBkKLwZoiAD0Yjb6woFt0PoKtf98hKbmO8bHkEP5NB5gSt4ergdB04t89/1O/w1cDnyilFU=',
-    'USER_ID': 'Ub2e428cb927dd64355c21fc95d6ada40'
+    "CHANNEL_ACCESS_TOKEN": "XuQz/EHfB2jodhYK4zZNvn6Wzual/YbnM8yLQGLmUR/cy7nsCC5uV2JN2bU4KCZzT7Yy2rO9yxfxY6isPyr2W6KQSReqfamLieRlxBkKLwZoiAD0Yjb6woFt0PoKtf98hKbmO8bHkEP5NB5gSt4ergdB04t89/1O/w1cDnyilFU=",
+    "USER_ID": "Ub2e428cb927dd64355c21fc95d6ada40"
 }
