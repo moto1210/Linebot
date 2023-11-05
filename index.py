224501360a5092d01311705ed9c3f546764804e8
apple <c1200922@st.kanazawa-it.ac.jp>
2023-11-05 09:47:11 +0900

    これは家計簿っとですというメッセージを送る

--- index.py
+++ index.py
@@ -1,3 +1,15 @@
 import json
-file = open("info.py", "r")
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+file = open("info.json", "r")
 info = json.load(file)
+CHANNEL_ACCESS_TOKEN = info['CHANNEL_ACCESS_TOKEN']
+Line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+
+def main():
+    USER_ID = info['USER_ID']
+    messages = TextSendMessage(text="これは家計簿っとです")
+    Line_bot_api.push_message(USER_ID,messages=messages)
+
+if __name__ == "__main__":
+    main()
